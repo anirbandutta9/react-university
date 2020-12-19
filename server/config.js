03654a84c61a255e99683078e0223d6f956b50c1
ANIRBAN DUTTA <anirbandutta011@gmail.com>
2020-12-19 18:06:07 +0530

    database connect

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
 
-    databaseURL: process.env.DATABASE_URL || "postgres://ccoenraets@localhost/university"
+    databaseURL: process.env.DATABASE_URL || "postgresql://university:wbye7vvlup0z920c@app-c1e630c7-f3c5-42e1-a729-9befac54ba4b-do-user-6938618-0.b.db.ondigitalocean.com:25060/university?sslmode=require"
 
 };
